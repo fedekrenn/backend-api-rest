0871a092b5e1aa6c4b3e9625a81187baede52f7b
Fede Krenn <fedekrenn@gmail.com>
2023-01-16 19:41:58 -0300

    Corrección de error en logger

--- src/middlewares/autorize.js
+++ src/middlewares/autorize.js
@@ -4,8 +4,8 @@ const authMiddleware = (req, res, next) => {
     if (req.headers.role === 'admin') {
         next();
     } else {
-        logger.warn(`Hubo un acceso en la ruta '${req.path}' método '${req.method}' no autorizada por falta de credenciales`);
-        res.send({ error: -1, descripcion: `ruta '${req.path}' método '${req.method}' no autorizada` }); 
+        loggerWarn(`Hubo un acceso en la ruta '${req.path}' método '${req.method}' no autorizada por falta de credenciales`);
+        res.send({ error: -1, descripcion: `ruta '${req.path}' método '${req.method}' no autorizada` });
     }
 }
 
